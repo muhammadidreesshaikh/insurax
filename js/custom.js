872fcbd5906911ea5b1c68964db06e778c6cf6f0
Muhammad Idrees <idreesiqbal104@gmail.com>
2023-09-15 23:27:49 +0500

    header complete

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+// nav-bar
+function toggleMenu(x) {
+  // icon animation
+  x.classList.toggle("change");
+
+  // menu open and close
+  document.querySelector(".sidenav").classList.toggle("active");
+}
